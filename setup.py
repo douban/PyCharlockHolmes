f840ab1d962bcaa29a5dcf3e0e65cf6db5e43933
XTao <xutao@douban.com>
2013-04-15 19:39:24 +0800

    Upload to PyPI

--- setup.py
+++ setup.py
@@ -3,13 +3,21 @@ import os
 
 # os.system('cd ./lib; ./build.sh')
 
-ch_exts = [os.path.join('src', name) for name in os.listdir('src')
-           if name.endswith('.c')]
+ch_exts = [
+    os.path.join('src', name) for name in os.listdir('src')
+    if name.endswith('.c')
+]
 
 # ch_module = Extension('charlockholmes', ch_exts, include_dirs=['./lib/magic/include'], library_dirs=['./lib/magic/lib'], libraries=['icui18n'])
 ch_module = Extension('charlockholmes', ch_exts, libraries=['icui18n', 'magic'])
 
-setup (name = 'charlockholmes',
-       version = '1.0',
-       description = 'Character encoding detecting library for Python using ICU and libmagic.',
-       ext_modules = [ch_module])
+setup (
+    name='charlockholmes',
+    version='0.0.1',
+    description='Character encoding detecting library for Python using ICU and libmagic.',
+    ext_modules=[ch_module],
+    keywords=('icu', 'magic', 'charlockholmes', 'egg'),
+    license='MIT License',
+    author='xtao',
+    author_email='xutao881001@gmail.com',
+)
