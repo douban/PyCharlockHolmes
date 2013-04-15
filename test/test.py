4ac58beb09422e65362c46482a584c3afbba0c68
XTao <xutao@douban.com>
2013-04-15 18:03:40 +0800

    Fix test error.

--- test/test.py
+++ test/test.py
@@ -8,7 +8,15 @@ from charlockholmes import detect
 TEST_FILES = {
     "py": [
         "file/test.py",
-        {'confidence': 34, 'type': 'text', 'language': 'en', 'encoding': 'ISO-8859-1'}
+        {'confidence': 43, 'type': 'text', 'language': 'en', 'encoding': 'ISO-8859-1'}
+    ],
+    "py1": [
+        "file/test1.py",
+        {'confidence': 80, 'type': 'text', 'encoding': 'UTF-8'}
+    ],
+    "lisp": [
+        "file/test.lisp",
+        {'confidence': 39, 'type': 'text', 'language': 'da', 'encoding': 'ISO-8859-1'}
     ],
     "txt": [
         "file/test.txt",
@@ -20,19 +28,19 @@ TEST_FILES = {
     ],
     "sh": [
         "file/test.sh",
-        {'confidence': 21, 'type': 'text', 'language': 'en', 'encoding': 'ISO-8859-1'}
+        {'confidence': 21, 'type': 'text', 'language': 'es', 'encoding': 'ISO-8859-1'}
     ],
     "elf": [
         "file/test",
-        {'confidence': 100, 'type': 'text'}
+        {'confidence': 100, 'type': 'binary'}
     ],
     "bz2": [
         "file/test.tar.bz2",
-        {'confidence': 100, 'type': 'text'}
+        {'confidence': 100, 'type': 'binary'}
     ],
     "gz": [
         "file/test.tar.gz",
-        {'confidence': 100, 'type': 'text'}
+        {'confidence': 100, 'type': 'binary'}
     ],
 }
 
