b262a66c025a58a3132ca6f9b199cc7414dfbc85
XTao <xutao@douban.com>
2013-04-17 14:04:46 +0800

    Add lib env.

--- setup.py
+++ setup.py
@@ -1,6 +1,19 @@
 from distutils.core import setup, Extension
 import os
 
+# Use environment variable ICUI18N to set your own icui18n configuration.
+icui18n_path = os.getenv("ICUI18N")
+if icui18n_path is None:
+    if os.name == 'nt':
+        program_files = os.getenv("ProgramFiles")
+        icui18n_path = '%s\icui18n' % program_files
+    else:
+        icui18n_path = '/usr/local'
+
+icui18n_bin = os.path.join(icui18n_path, 'bin')
+icui18n_include = os.path.join(icui18n_path, 'include')
+icui18n_lib = os.getenv('ICUI18N_LIB', os.path.join(icui18n_path, 'lib'))
+
 # os.system('cd ./lib; ./build.sh')
 
 ch_exts = [
@@ -9,7 +22,11 @@ ch_exts = [
 ]
 
 # ch_module = Extension('charlockholmes', ch_exts, include_dirs=['./lib/magic/include'], library_dirs=['./lib/magic/lib'], libraries=['icui18n'])
-ch_module = Extension('charlockholmes', ch_exts, libraries=['icui18n', 'magic'])
+ch_module = Extension('charlockholmes',
+                      ch_exts,
+                      include_dirs=[icui18n_include, 'include'],
+                      library_dirs=[icui18n_lib],
+                      libraries=['icui18n', 'magic'])
 
 setup (
     name='charlockholmes',
